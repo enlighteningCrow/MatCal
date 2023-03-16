2e255803b1f276bcd575e5e2458325dd98a6c18c
enlighteningCrow <87510657+enlighteningCrow@users.noreply.github.com>
2023-03-16 11:43:27 +0700

    Updated compileUi to be compatible with windows

--- uiCompiler.py
+++ uiCompiler.py
@@ -8,20 +8,24 @@ from PySide6.QtCore import QDirIterator
 
 
 def compileUi():
-    uicPath = Path(PySide6.__file__).parent / "Qt" / "libexec" / "uic"
-    if not uicPath.exists():
-        uicPath = Path(PySide6.__file__).parent / "Qt" / "libexec" / "uic"
-        print("uic executable not found at path: \"" + str(uicPath))
+    uicPaths = [
+        Path(PySide6.__file__).parent / "Qt" / "libexec" / "uic",
+        Path(PySide6.__file__).parent / "uic.exe"
+    ]
+    for uicPath in uicPaths:
+        if not uicPath.exists():
+            print("uic executable not found at path: \"" + str(uicPath))
+            continue
+        print("uic executable exists at path: \"" + str(uicPath))
+        for i in glob("*.ui"):
+            path = Path(i)
+            print(
+                "Compiling " + str(path) + " to " +
+                str(path.parent / ("ui_" + path.stem + ".py"))
+            )
+            os.system(
+                str(uicPath) + " " + str(path) + " -g python -o " +
+                str(path.parent / ("ui_" + path.stem + ".py"))
+            )
         return
-        # sys.exit(1)
-    print("uic executable exists at path: \"" + str(uicPath))
-    for i in glob("*.ui"):
-        path = Path(i)
-        print(
-            "Compiling " + str(path) + " to " +
-            str(path.parent / ("ui_" + path.stem + ".py"))
-        )
-        os.system(
-            str(uicPath) + " " + str(path) + " -g python -o " +
-            str(path.parent / ("ui_" + path.stem + ".py"))
-        )
+    sys.exit(1)
